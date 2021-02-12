2e5287dd63b5c800f575b486fbc51ea32cba972c
Jaime Rivera <riverac85@gmail.com>
2021-02-11 20:58:14 -0500

    se comenta vista 404

--- src/app.js
+++ src/app.js
@@ -23,9 +23,9 @@ app.use(express.json());
 
 app.use(methodOverride("_method"));
 
-app.use((req, res, next) => {
-    res.status(404).render("not-found");
-})
+// app.use((req, res, next) => {
+//     res.status(404).render('pages/not-found');
+// })
 
 //Routes
 const mainRouter = require('./routes/main');
