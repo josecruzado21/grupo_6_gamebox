ee4b1f97e3d36848d798eed1d5dcaeeeed877d90
Juan Castaneda <juan.castaneda@uproar.car>
2021-02-16 11:55:22 -0500

    ajustes en el header

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,37 +1,37 @@
-  
+
 const path = require('path');
 let productsController = {
     product: (req, res) => {
         let title = 'Gamebox | Producto ';
-     
+
         res.render('pages/products/productDetail', {
             'title': title
         })
     },
 
     list: (req, res) => {
-       
+
         let title = 'Gamebox | Lista de Productos ';
-        
-         res.render('pages/products/productList', {
-             'title': title
-         })
+
+        res.render('pages/products/productList', {
+            'title': title
+        })
     },
     create: (req, res) => {
-       
+
         let title = 'Gamebox | Crear Producto ';
-     
-         res.render('pages/products/productCreate', {
-             'title': title
-         })
+
+        res.render('pages/products/productCreate', {
+            'title': title
+        })
     },
     edit: (req, res) => {
-       
+
         let title = 'Gamebox |Editar Producto ';
-       
-         res.render('pages/products/productEdit', {
-             'title': title
-         })
+
+        res.render('pages/products/productEdit', {
+            'title': title
+        })
     },
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,4 +1,3 @@
-  
 const express = require('express');
 const productsController = require('../controllers/productsController');
 
