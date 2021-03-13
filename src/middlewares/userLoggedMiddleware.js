b97f50dd373ce94e324dbecb3e79724eea575d89
Jaime Rivera <riverac85@gmail.com>
2021-03-13 14:48:30 -0500

    fix error cookie

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -4,13 +4,18 @@ function userLoggedMiddleware(req, res, next){
 
     res.locals.isLogged = false;
 
-    
+    console.log(req.cookies);
+    if(req.cookies){
+
     let emailInCookie = req.cookies.userEmail;
     let userFromCookie = User.findByProperty('email', emailInCookie);
-
     if(userFromCookie){
         req.session.userLogged = userFromCookie;
     }
+
+    }
+
+
 
     if(req.session &&  req.session.userLogged){
         res.locals.isLogged = true;
